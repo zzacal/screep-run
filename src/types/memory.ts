7003d5bd719d653b1e17c27cad99deaf1aaa0dfd
zzacal <jizacal@gmail.com>
2021-08-30 08:32:22 -0700

    Added roles

---
+++ src/types/memory.ts
@@ -0,0 +1,4 @@
+export interface CreepMemory { building: boolean; };
+export interface FlagMemory { [name: string]: any };
+export interface SpawnMemory { [name: string]: any };
+export interface RoomMemory { [name: string]: any };
